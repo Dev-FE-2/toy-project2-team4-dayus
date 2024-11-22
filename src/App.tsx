b4c9dc569239cb0eb74d1aed2c838fcecca721c7
bbjbc <jobc4550@gmail.com>
2024-11-22 22:11:45 +0900

    chore: 프로젝트 셋팅

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <h1>Day Us</h1>
+      <h2>
+        카페 크루의 일상적인 고민을 해결하는 스마트한 근무 관리 플랫폼입니다.
+      </h2>
+    </>
+  );
+};
+
+export default App;
