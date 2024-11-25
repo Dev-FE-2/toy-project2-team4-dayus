50eb45775c0ed16822adce77462e22cb88e81eaa
bbjbc <jobc4550@gmail.com>
2024-11-26 00:35:47 +0900

    refactor: 절대 경로로 인한 경로 수정 및 에러 페이지 위치 변경

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import HomePage from '../pages/HomePage';
-import LoginPage from '../pages/LoginPage';
-import ProfilePage from '../pages/ProfilePage';
-import SalaryPage from '../pages/SalaryPage';
-import NotFoundPage from '../pages/NotFoundPage';
-import Layout from '../layout/Layout';
-import { ROUTER_PATH } from '../constants/constant';
+import HomePage from '@/pages/HomePage';
+import LoginPage from '@/pages/LoginPage';
+import ProfilePage from '@/pages/ProfilePage';
+import SalaryPage from '@/pages/SalaryPage';
+import NotFoundPage from '@/pages/NotFoundPage';
+import Layout from '@/layout/Layout';
+import { ROUTER_PATH } from '@/constants/constant';
 
 const Router = () => {
   const { HOME, LOGIN, PROFILE, SALARY } = ROUTER_PATH;
@@ -14,12 +14,12 @@ const Router = () => {
   const router = createBrowserRouter([
     {
       element: <Layout />,
-      errorElement: <NotFoundPage />,
       children: [
         { path: HOME, element: <HomePage /> },
         { path: LOGIN, element: <LoginPage /> },
         { path: PROFILE, element: <ProfilePage /> },
         { path: SALARY, element: <SalaryPage /> },
+        { path: '*', element: <NotFoundPage /> },
       ],
     },
   ]);
