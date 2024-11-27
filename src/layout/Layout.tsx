dd906c63ca4c75f7946e3e84ca06412ea7a16216
yulsanoh <yulsanohdev@gmail.com>
2024-11-27 15:53:43 +0900

    Revert "Merge pull request #15 from Dev-FE-2/feat/navbar-component"
    This reverts commit c06374d321974f50a977fac1d55c908a071cf59c, reversing
    changes made to 73991a4034183d1fd13c3a009efa053d661a03c5.

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,13 +1,11 @@
 import { Outlet } from 'react-router-dom';
 
-import NavBar from '@/components/ui/Navbar';
 import * as S from './Layout.styles';
 
 const Layout = () => {
   return (
     <S.Container>
       <Outlet />
-      <NavBar />
     </S.Container>
   );
 };
