b9b8460387e8f9e20d827ad3392a4a88d08ca67d
laivastia <zmfwl123@gmail.com>
2024-11-28 14:51:32 +0900

    style: tab styled-component의 css를 토큰 변수로 변경

--- src/components/ui/Tab/Tab.styles.ts
+++ src/components/ui/Tab/Tab.styles.ts
@@ -19,11 +19,13 @@ export const TabItem = styled.li`
 `;
 
 export const TabButton = styled.button<ITabButton>`
-  font-size: 16px;
+  font-size: var(--font-base);
   font-weight: bold;
-  padding: 4px;
+  padding: var(--spacing-1);
   // 탭 활성화시 검은색 텍스트 | 비활성화시 회색 텍스트
-  color: ${props => (props.$active ? 'black' : '#757575')};
+  color: ${props =>
+    props.$active ? 'var(--color-black)' : 'var(--color-gray-600)'};
   // 탭 활성화시 검은색 아래 테두리 | 비활성화시 테두리 없음
-  border-bottom: ${props => (props.$active ? '1px solid black' : 'none')};
+  border-bottom: ${props =>
+    props.$active ? '1px solid var(--color-black)' : '1px solid transparent'};
 `;
