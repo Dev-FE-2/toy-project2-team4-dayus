d698c5aa69e16a8304235b1b7f6dbbc3e8cba436
laivastia <zmfwl123@gmail.com>
2024-11-29 14:24:46 +0900

    style: 페이지 뒤로가기 버튼 UI 스타일 컴포넌트 생성

---
+++ src/components/ui/PageNav/PageNav.styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const PageNavBox = styled.div`
+  width: 100%;
+  height: 5.8rem;
+  padding: 1rem 1rem 0;
+`;
+
+export const PageNavButton = styled.button`
+  padding: 1rem;
+  border: none;
+  outline: none;
+  display: flex;
+  align-items: center;
+  gap: var(--spacing-4);
+`;
+
+export const PageNavTitle = styled.span`
+  font-size: var(--font-xl);
+  flex: 1;
+`;
