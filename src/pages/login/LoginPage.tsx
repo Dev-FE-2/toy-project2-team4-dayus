163d3daf4841a071d2fc7cf94ba70871592139e2
yulsanoh <yulsanohdev@gmail.com>
2024-11-29 17:25:18 +0900

    feat: 로그인 페이지 UI 구현

---
+++ src/pages/login/LoginPage.tsx
@@ -0,0 +1,66 @@
+import styled from 'styled-components';
+import logo from '../../assets/logo.svg';
+import Input from '@/components/ui/Input';
+import Button from '@/components/ui/Button/Button';
+
+const LoginPage = () => {
+  return (
+    <LoginWrapper>
+      <LoginLogoWrapper>
+        <LoginLogo src={logo}></LoginLogo>
+      </LoginLogoWrapper>
+      <LoginForm>
+        <LoginFormItem>
+          <LoginLabel htmlFor="email">이메일</LoginLabel>
+          <Input placeholder="example@day.us" id="email" type="text" />
+        </LoginFormItem>
+        <LoginFormItem>
+          <LoginLabel htmlFor="password">패스워드</LoginLabel>
+          <Input placeholder="Value" id="password" type="password" />
+        </LoginFormItem>
+        <Button $size="lg">로그인</Button>
+      </LoginForm>
+    </LoginWrapper>
+  );
+};
+
+const LoginWrapper = styled.div`
+  min-height: calc(100vh - var(--navbar-height));
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+`;
+
+const LoginLogoWrapper = styled.div`
+  width: 100%;
+  text-align: center;
+`;
+const LoginLogo = styled.img`
+  width: 100%;
+`;
+
+const LoginForm = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 28px;
+
+  & > :last-child {
+    margin-top: 20px;
+  }
+`;
+
+const LoginFormItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  gap: 10px;
+`;
+
+const LoginLabel = styled.label`
+  font-size: var(--font-lg);
+`;
+
+export default LoginPage;
