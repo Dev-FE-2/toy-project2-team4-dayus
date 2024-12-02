93158d5f3e848a2a01c3f168b518a32fe60fcfa8
KwonSeami <kwonsam07@daum.net>
2024-12-02 15:08:04 +0900

    feat: makeRandomRange 함수 예외 처리 추가 #13

--- src/utils/makeRandomRange.ts
+++ src/utils/makeRandomRange.ts
@@ -1,3 +1,11 @@
-const makeRandomRange = (min: number, max: number): number =>
-  Math.floor(Math.random() * (max - min + 1)) + min;
+/**
+ * @param min - 최소값
+ * @param max - 최대값
+ */
+const makeRandomRange = (min: number, max: number): number => {
+  if (min > max) {
+    throw new Error('makeRandomRange error: min, max 값을 확인해 주세요.');
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 export default makeRandomRange;
