e585fec8e26be64897544681371fdd1b89b25bb2
yulsanoh <yulsanohdev@gmail.com>
2024-12-03 13:57:06 +0900

    feat: firebase를 이용해 로그인 기능 추가

---
+++ src/apis/firebase/firestore.ts
@@ -0,0 +1,8 @@
+import app from './firebase';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
+const auth = getAuth(app);
+
+export const signIn = async (email: string, password: string) => {
+  return signInWithEmailAndPassword(auth, email, password);
+};
--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,3 +1,7 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { FirebaseError } from 'firebase/app';
+import { signIn } from '@/apis/firebase/firestore';
 import logo from '../../assets/logo.svg';
 import Input from '@/components/ui/Input';
 import Button from '@/components/ui/Button/Button';
@@ -5,23 +9,77 @@ import LabeledBox from '@/components/ui/Label/LabeledBox';
 import * as S from './LoginPage.styles';
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const onSubmitLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (isLoading || email === '' || password === '') return;
+
+    try {
+      setIsLoading(true);
+      await signIn(email, password);
+
+      navigate('/');
+    } catch (error) {
+      if (error instanceof FirebaseError) {
+        setError('이메일과 비밀번호를 확인해주세요.');
+
+        console.log(error.code);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+
+    console.log(email, password);
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  };
+
   return (
     <S.LoginWrapper>
       <S.LoginLogoWrapper>
         <S.LoginLogo src={logo}></S.LoginLogo>
       </S.LoginLogoWrapper>
-      <S.LoginForm>
+      <S.LoginForm onSubmit={onSubmitLogin}>
         <S.LoginFormItem>
           <LabeledBox id="email" text="이메일">
-            <Input placeholder="이메일을 입력해 주세요" />
+            <Input
+              onChange={onChange}
+              name="email"
+              placeholder="이메일을 입력해 주세요"
+              value={email}
+            />
           </LabeledBox>
         </S.LoginFormItem>
         <S.LoginFormItem>
           <LabeledBox id="password" text="비밀번호">
-            <Input placeholder="비밀번호를 입력해 주세요" type="password" />
+            <Input
+              onChange={onChange}
+              name="password"
+              placeholder="비밀번호를 입력해 주세요"
+              type="password"
+              value={password}
+            />
           </LabeledBox>
         </S.LoginFormItem>
-        <Button $size="lg">로그인</Button>
+        {error && <S.LoginFormAlert>{error}</S.LoginFormAlert>}
+        <Button disabled={isLoading} $size="lg">
+          {isLoading ? '로그인 중..' : '로그인'}
+        </Button>
       </S.LoginForm>
     </S.LoginWrapper>
   );
