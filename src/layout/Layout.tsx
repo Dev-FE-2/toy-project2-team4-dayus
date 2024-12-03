e2c33c544643a9e11a248c2abd8d851e92c26997
bbjbc <jobc4550@gmail.com>
2024-12-03 12:27:58 +0900

    feat: header 컴포넌트 추가

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -2,10 +2,12 @@ import { Outlet } from 'react-router-dom';
 
 import NavBar from '@/components/ui/Navbar';
 import * as S from './Layout.styles';
+import Header from '@/components/header/Header';
 
 const Layout = () => {
   return (
     <S.Container>
+      <Header />
       <Outlet />
       <NavBar />
     </S.Container>
