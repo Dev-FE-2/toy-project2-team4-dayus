53279c88ad9e7b01b38e7e8af444eca1f54fd40f
bbjbc <jobc4550@gmail.com>
2024-12-04 18:23:45 +0900

    chore: 파일 구조 변경

--- src/mocks/data/calendar.ts
+++ src/mocks/data/calendar.ts
@@ -56,3 +56,30 @@ export const eventList = [
     // allDay: true
   },
 ];
+
+export const mockSchedules = [
+  {
+    id: '1',
+    scheduleTitle: '프로모션 주간',
+    scheduleDate: '12월 4일 ~ 12월 6일',
+    color: '#7AFE42',
+  },
+  {
+    id: '2',
+    scheduleTitle: '마감',
+    scheduleDate: '12월 4일',
+    color: '#FEBC42',
+  },
+  {
+    id: '3',
+    scheduleTitle: '재고정리',
+    scheduleDate: '12월 4일 ~ 12월 6일',
+    color: '#42E5FE',
+  },
+  {
+    id: '4',
+    scheduleTitle: '출입문 청소',
+    scheduleDate: '12월 1일 ~ 12월 6일',
+    color: '#955A53',
+  },
+];
