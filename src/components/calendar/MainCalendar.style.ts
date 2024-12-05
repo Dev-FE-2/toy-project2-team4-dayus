ed51ff94467c58331dd00e946c20206389f8c454
bbjbc <jobc4550@gmail.com>
2024-12-05 14:32:17 +0900

    feat: 달력 셀 클릭 시 해당 내용 모달에 리스트업 되도록 변경

--- src/components/calendar/MainCalendar.style.ts
+++ src/components/calendar/MainCalendar.style.ts
@@ -19,6 +19,7 @@ export const Container = styled.div`
   }
 
   .rbc-month-row {
+    cursor: pointer;
     .rbc-row-content {
       .rbc-date-cell {
         &:first-child {
@@ -35,34 +36,4 @@ export const Container = styled.div`
       background: var(--color-gray-100);
     }
   }
-`;
-
-export const CalendarHeader = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0 0 1rem;
-`;
-
-export const CalendarHeaderInner = styled.div`
-  display: flex;
-  align-items: center;
-
-  button {
-    padding: 0.2rem;
-    font-size: 0;
-  }
-`;
-
-export const NowMonthLabel = styled.span`
-  width: 12.2rem;
-  font-size: var(--font-xl);
-  font-weight: 700;
-`;
-
-export const TodayButton = styled.button`
-  padding: 0.4rem 0.8rem;
-  background: var(--color-main);
-  color: var(--color-white);
-  border-radius: var(--radius-base);
 `;
