e0e78331dddeb5824b5ac6a430a3f2068635dbd4
yulsanoh <yulsanohdev@gmail.com>
2024-12-06 19:46:44 +0900

    feat: redux 설치 후 userStore 생성

---
+++ src/store/userStore.ts
@@ -0,0 +1,55 @@
+import { auth } from '@/server/firebase/auth';
+import { configureStore, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { onAuthStateChanged } from 'firebase/auth';
+
+export interface IUserState {
+  uid: string | null;
+  email: string | null;
+  emailVerified: boolean | null;
+  providerData: object[];
+}
+
+const initialState: IUserState = {
+  uid: null,
+  email: null,
+  emailVerified: null,
+  providerData: [],
+};
+
+const userSlice = createSlice({
+  name: 'userReducer',
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<IUserState>) => {
+      return action.payload;
+    },
+    clearUser: () => initialState,
+  },
+});
+
+const userStore = configureStore({ reducer: userSlice.reducer });
+
+export type RootState = ReturnType<typeof userStore.getState>;
+
+export const { setUser, clearUser } = userSlice.actions;
+export default userStore;
+
+export const initializeAuth = (): Promise<void> => {
+  return new Promise(resolve => {
+    onAuthStateChanged(auth, user => {
+      if (user) {
+        userStore.dispatch(
+          setUser({
+            uid: user.uid,
+            email: user.email,
+            emailVerified: user.emailVerified,
+            providerData: user.providerData,
+          }),
+        );
+      } else {
+        userStore.dispatch(clearUser());
+      }
+      resolve();
+    });
+  });
+};
