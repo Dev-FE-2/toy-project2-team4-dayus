a2dd3ccc74a9ebd547fd017e9a5fcb32d24a53fd
laivastia <zmfwl123@gmail.com>
2024-12-06 13:55:24 +0900

    feat: msw mock api 개발

---
+++ src/mocks/data/salary.ts
@@ -0,0 +1,90 @@
+export const salaryList = [
+  {
+    salarySn: 'salary_0',
+    title: '8월 급여',
+    paymentDate: new Date('2024-10-17'),
+    totalAmount: 457871,
+  },
+  {
+    salarySn: 'salary_1',
+    title: '9월 급여',
+    paymentDate: new Date('2024-09-17'),
+    totalAmount: 461776,
+  },
+  {
+    salarySn: 'salary_2',
+    title: '10월 급여',
+    paymentDate: new Date('2024-08-18'),
+    totalAmount: 440165,
+  },
+  {
+    salarySn: 'salary_3',
+    title: '11월 급여',
+    paymentDate: new Date('2024-07-19'),
+    totalAmount: 404873,
+  },
+  {
+    salarySn: 'salary_4',
+    title: '12월 급여',
+    paymentDate: new Date('2024-06-19'),
+    totalAmount: 222372,
+  },
+  {
+    salarySn: 'salary_5',
+    title: '1월 급여',
+    paymentDate: new Date('2024-05-20'),
+    totalAmount: 115008,
+  },
+  {
+    salarySn: 'salary_6',
+    title: '2월 급여',
+    paymentDate: new Date('2024-04-20'),
+    totalAmount: 359546,
+  },
+  {
+    salarySn: 'salary_7',
+    title: '3월 급여',
+    paymentDate: new Date('2024-03-21'),
+    totalAmount: 255663,
+  },
+  {
+    salarySn: 'salary_8',
+    title: '4월 급여',
+    paymentDate: new Date('2024-02-20'),
+    totalAmount: 117334,
+  },
+  {
+    salarySn: 'salary_9',
+    title: '5월 급여',
+    paymentDate: new Date('2024-01-21'),
+    totalAmount: 149197,
+  },
+  {
+    salarySn: 'salary_10',
+    title: '6월 급여',
+    paymentDate: new Date('2023-12-22'),
+    totalAmount: 240554,
+  },
+  {
+    salarySn: 'salary_11',
+    title: '7월 급여',
+    paymentDate: new Date('2023-11-22'),
+    totalAmount: 189525,
+  },
+];
+
+export const salaryData = {
+  salarySn: 'salary_12',
+  tour: {
+    startDate: new Date('2024-07-15'),
+    endDate: new Date('2024-07-20'),
+  },
+  paymentDate: new Date('2024-08-05'),
+  bank: {
+    name: '신한은행',
+    account: '110-123-456789',
+  },
+  deductible: 50000,
+  amount: 1500000,
+  totalAmount: 1450000,
+};
