ea378665f83318079c0633e49442c33c7cedf942
bbjbc <jobc4550@gmail.com>
2024-12-06 12:50:14 +0900

    feat: 휴지통 아이콘 클릭 시 확인 버튼 추가

--- src/components/schedule/schedule-item/ScheduleItem.tsx
+++ src/components/schedule/schedule-item/ScheduleItem.tsx
@@ -1,30 +1,59 @@
+import { useState } from 'react';
+
 import { GoTrash } from 'react-icons/go';
 
 import * as S from './ScheduleItem.styles';
+import Button from '@/components/ui/Button/Button';
 import { ScheduleItemProps } from '@/types/schedule';
 import { formatDateRange } from '@/utils/formatDate';
 
 const ScheduleItem = ({ schedule, onDelete }: ScheduleItemProps) => {
+  const [showDeleteButton, setShowDeleteButton] = useState(false);
+
   if (!schedule) return null;
 
-  return (
-    <S.ScheduleItem>
-      <S.ScheduleInfo>
-        <S.ColorDot $bgColor={schedule.color.bgColor} />
-        <S.ScheduleText>
-          <S.Title>{schedule.title}</S.Title>
-          <S.Date>
-            {schedule.start &&
-              schedule.end &&
-              formatDateRange(schedule.start, schedule.end)}
-          </S.Date>
-        </S.ScheduleText>
-      </S.ScheduleInfo>
+  const handleDelete = () => {
+    onDelete(schedule.eventId);
+    setShowDeleteButton(false);
+  };
 
-      <S.DeleteButton onClick={() => onDelete(schedule.eventId)}>
-        <GoTrash size={24} />
-      </S.DeleteButton>
-    </S.ScheduleItem>
+  const handleCancel = () => {
+    setShowDeleteButton(false);
+  };
+
+  const toggleTrashButton = () => {
+    setShowDeleteButton(prev => !prev);
+  };
+
+  return (
+    <S.ScheduleItemWrapper>
+      <S.ScheduleItem>
+        <S.ScheduleInfo>
+          <S.ColorDot $bgColor={schedule.color.bgColor} />
+          <S.ScheduleText>
+            <S.Title>{schedule.title}</S.Title>
+            <S.Date>
+              {schedule.start &&
+                schedule.end &&
+                formatDateRange(schedule.start, schedule.end)}
+            </S.Date>
+          </S.ScheduleText>
+        </S.ScheduleInfo>
+
+        <S.DeleteButton onClick={toggleTrashButton}>
+          <GoTrash size={24} />
+        </S.DeleteButton>
+      </S.ScheduleItem>
+
+      <S.DeleteConfirmation $show={showDeleteButton}>
+        <Button onClick={handleDelete} $variant="danger" $size="sm">
+          삭제하기
+        </Button>
+        <Button onClick={handleCancel} $variant="secondary" $size="sm">
+          취소
+        </Button>
+      </S.DeleteConfirmation>
+    </S.ScheduleItemWrapper>
   );
 };
 
