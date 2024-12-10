c7a6bf208b0eab09ea2b698732f6071293fa0e91
laivastia <zmfwl123@gmail.com>
2024-12-11 02:47:44 +0900

    feat: 근무 정정 유형과 근무 정정 승인 상태 선택을 위한 상수 생성

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -62,9 +62,15 @@ export const SALARY_DETAIL_KEY = {
 export const ADD_SCHEDULE_MODAL_ID = 'add-schedule-modal';
 
 export const SELECT_WORK_TYPE = [
-  { label: '전체', value: 0 },
-  { label: '정규 근무', value: 1 },
-  { label: '대리 근무', value: 2 },
-  { label: '연장 근무', value: 3 },
-  { label: '추가 근무', value: 4 },
+  { label: '근무 정정 유형', value: '' },
+  { label: '정규 근무', value: '정규 근무' },
+  { label: '대리 근무', value: '대리 근무' },
+  { label: '연장 근무', value: '연장 근무' },
+  { label: '추가 근무', value: '추가 근무' },
+];
+
+export const SELECT_APPROVAL_TYPE = [
+  { label: '승인 상태', value: '' },
+  { label: '승인', value: '승인' },
+  { label: '비승인', value: '비승인' },
 ];
