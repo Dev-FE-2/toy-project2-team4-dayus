0ccb88a0369955c4cdb6097650cf9b17464d6207
laivastia <zmfwl123@gmail.com>
2024-12-11 10:02:21 +0900

    feat: 근무 정정 페이지 개발 1. 데이터 중복을 방지하기위한 데이터 수정 유틸 함수 추가

--- src/pages/shift/ShiftPage.tsx
+++ src/pages/shift/ShiftPage.tsx
@@ -1,17 +1,26 @@
 import ShiftList from '@/components/list/ShiftList';
 import Select from '@/components/ui/Select';
 import { SELECT_APPROVAL_TYPE, SELECT_WORK_TYPE } from '@/constants/constant';
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import * as S from './ShiftPage.styles';
+import { getShiftList } from '@/api/shiftApi';
+import { IShiftList, ShiftListItem } from '@/types/shift';
+import { removeDuplicates } from '@/utils/arrayUtils';
 
 const ShiftPage = () => {
-  const [workType, setWorkType] = useState('0');
-  const [approvalType, setApprovalType] = useState('0');
-  const [isLoading] = useState(false);
+  const [workType, setWorkType] = useState('');
+  const [approvalType, setApprovalType] = useState('');
+  const [page, setPage] = useState(0);
+  const [shiftList, setShiftList] = useState<ShiftListItem[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasLoadMore, setHasLoadMore] = useState(true);
 
   const handleWorkChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     if (isLoading) return;
     setWorkType(event.target.value);
+    setShiftList([]);
+    setPage(0);
+    setHasLoadMore(true);
   };
 
   const handleApprovalChange = (
@@ -19,6 +28,43 @@ const ShiftPage = () => {
   ) => {
     if (isLoading) return;
     setApprovalType(event.target.value);
+    setShiftList([]);
+    setPage(0);
+    setHasLoadMore(true);
+  };
+
+  const fetchShiftList = useCallback(async () => {
+    if (isLoading || !hasLoadMore) return;
+    setIsLoading(true);
+    try {
+      const response: IShiftList = await getShiftList(
+        page,
+        10,
+        workType,
+        approvalType,
+      );
+      const { currentPage, data, totalPage } = response;
+      setShiftList(prevList => [
+        ...prevList,
+        ...removeDuplicates(prevList, data, 'shiftSn'),
+      ]);
+      if (currentPage + 1 >= totalPage) setHasLoadMore(false);
+      return response;
+    } catch (error) {
+      throw new Error(`error: ${error}`);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [isLoading, hasLoadMore, page, workType, approvalType]);
+
+  useEffect(() => {
+    fetchShiftList();
+  }, [fetchShiftList]);
+
+  const handleScroll = () => {
+    if (hasLoadMore) {
+      setPage(prev => prev + 1);
+    }
   };
 
   return (
@@ -35,10 +81,8 @@ const ShiftPage = () => {
       />
       <ShiftList
         isLoading={isLoading}
-        listItem={[]}
-        onLoadMore={() => {
-          return;
-        }}
+        listItem={shiftList}
+        onLoadMore={handleScroll}
       />
     </S.Container>
   );
