78f134ee9f1de9fcb4011a993eab154b16ee17f2
bbjbc <jobc4550@gmail.com>
2024-12-12 11:45:24 +0900

    feat: 캘린더 API mock handler 추가

--- src/server/handlers/index.ts
+++ src/server/handlers/index.ts
@@ -1,9 +1,11 @@
 import { exampleHandlers } from './exampleHandler';
 import { salaryHandlers } from './salaryHandlers';
 import { shiftHandlers } from './shiftHandler';
+import { calendarHandlers } from './calendarHandlers';
 
 export const apiHandlers = [
   ...exampleHandlers,
   ...salaryHandlers,
   ...shiftHandlers,
+  ...calendarHandlers,
 ];
