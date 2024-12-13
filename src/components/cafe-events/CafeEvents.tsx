2bedd87e5caed169378d1def74cf840f88a4fa68
bbjbc <jobc4550@gmail.com>
2024-12-13 18:44:39 +0900

    refactor: 카페 행사 이벤트 카드 컴포넌트로 분리리

--- src/components/cafe-events/CafeEvents.tsx
+++ src/components/cafe-events/CafeEvents.tsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from 'react';
 
-import dayjs from 'dayjs';
-
 import * as S from './CafeEvents.styles';
+import CafeEventCard from './CafeEventCard';
 import Spinner from '../ui/Spinner';
 import { getCafeScheduleItems } from '@/api/cafeScheduleApi';
 import { ICafeEventList } from '@/types/calendar';
@@ -43,13 +42,7 @@ const CafeEvents = () => {
           <Spinner size={40} />
         ) : cafeEvents.length > 0 ? (
           cafeEvents.map(event => (
-            <S.EventCard key={event.eventId}>
-              <S.EventDate>
-                {dayjs(event.start).format('YYYY년 M월 D일')} ~{' '}
-                {dayjs(event.end).format('YYYY년 M월 D일')}
-              </S.EventDate>
-              <S.EventTitle>{event.title}</S.EventTitle>
-            </S.EventCard>
+            <CafeEventCard key={event.eventId} {...event} />
           ))
         ) : (
           <S.NoEvents>현재 예정된 카페 행사가 없습니다.</S.NoEvents>
